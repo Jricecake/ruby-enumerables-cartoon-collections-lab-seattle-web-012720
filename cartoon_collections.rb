729e76aa0681de80c942d48b91f40a071cac1239
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-21 00:39:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+ array.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
+end
+
+def summon_captain_planet(array)
+  array.collect { |x| "#{x.capitalize}!" }
+end
+
+def long_planeteer_calls(array)
+  array.any? { |word| word.length > 4 }
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? { |item| cheese_types.include?(item) }
+  if true
+    return item
 end
